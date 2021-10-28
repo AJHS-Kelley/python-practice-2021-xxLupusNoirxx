0387bb467799e2971c96f53541de3f7827bea6ea
Kenyon S <90405891+xxLupusNoirxx@users.noreply.github.com>
2021-10-28 12:06:38 -0400

    Create variables_practice.py
    Added More infomations

---
+++ variables/variables_practice.py
@@ -0,0 +1,11 @@
+#Variable Practice, Kenyon Smith, 10/28/21 11:39, version 0.0
+
+# Variables Types -- Most Commonn
+
+my float = 76.544575
+my int = -673672
+my string = "I Like Dragons"
+
+print(my_float)
+print(my_int)
+print(string)
